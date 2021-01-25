3479b3391e31a63c5ddede49c9917a10bbd143e3
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-01-25 18:37:01 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLine = Console.ReadLine();
+            var secondLine = Console.ReadLine();
+            var result = "";
+
+            for (int i = 0; i < firstLine.Length; i++)
+            {
+                if (firstLine[i] == secondLine[i])
+                {
+                    result = result + 0;
+                }
+                else
+                {
+                    result = result + 1;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
+
