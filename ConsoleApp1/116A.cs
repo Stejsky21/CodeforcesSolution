a4798a0f6cac94a70254f7c2f25a9993e6738113
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-01-25 19:23:28 +0100

    Added solution 4A

---
+++ ConsoleApp1/116A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codeforces
+{
+    class Program116A
+    {
+        public static void Solve()
+        {
+            Console.WriteLine("Solved!");
+        }
+    }
+}
