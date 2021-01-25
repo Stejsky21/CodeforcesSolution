01bcd86cd1efcc8bea7a2ab771577b9f96edf4be
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-01-25 18:59:22 +0100

    Added solution for 977A

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,23 +7,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var firstLine = Console.ReadLine();
-            var secondLine = Console.ReadLine();
-            var result = "";
-
-            for (int i = 0; i < firstLine.Length; i++)
-            {
-                if (firstLine[i] == secondLine[i])
-                {
-                    result = result + 0;
-                }
-                else
-                {
-                    result = result + 1;
-                }
-            }
-
-            Console.WriteLine(result);
         }
     }
 }
