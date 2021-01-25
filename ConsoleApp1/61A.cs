3c0f9400f672e23c0e2843bdbdcaef3c5b3ed7c0
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-01-25 18:44:09 +0100

    Updated 61A

--- ConsoleApp1/61A.cs
+++ ConsoleApp1/61A.cs
@@ -3,14 +3,16 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
+    //class Program
+    //{
+    //    static void Main(string[] args)
     class Program61A
     {
         static void Solution61A(string[] args)
         {
             var firstLine = Console.ReadLine();
             var secondLine = Console.ReadLine();
-            var result = "";
-
+            var result = string.Empty;
             for (int i = 0; i < firstLine.Length; i++)
             {
                 if (firstLine[i] == secondLine[i])
