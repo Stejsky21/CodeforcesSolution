62738dc71ad7c88071586d923a9daf2686eb4d16
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-01-25 19:02:56 +0100

    Fixed project organization

---
+++ ConsoleApp1/977A.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    class Program977A
+    {
+        static void Main977A(string[] args)
+        {
+            var firstLine = Console.ReadLine().Split(' ');
+            var number = int.Parse(firstLine[0]);
+            var numberOfSubstractions = int.Parse(firstLine[1]);
+
+            for (int i = 0; i < numberOfSubstractions; i++)
+            {
+                if (number % 10 == 0)
+                {
+                    number = number / 10;
+                }
+                else
+                {
+                    number--;
+                }
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
