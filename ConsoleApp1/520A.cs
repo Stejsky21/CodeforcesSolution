f8d428a25f9dcbe90ff1e4eadfc547c4a1e2506a
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-02-02 12:57:58 +0100

    Added solution 520A

--- ConsoleApp1/520A.cs
+++ ConsoleApp1/520A.cs
@@ -3,9 +3,12 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
-    class Program
+    //class Program
+    //{
+    //static void Main(string[] args)
+    class Program520A
     {
-        static void Main(string[] args)
+        static void Main520A(string[] args)
         {
             var firstLine = Console.ReadLine();
             var abeceda = "abcdefghijklmnopqrstuvwxyz";
