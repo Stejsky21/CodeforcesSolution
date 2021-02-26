ed6d9ee22e8e34bc657832e18df82157efc92e05
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-02-26 13:34:35 +0100

    Added solution 32B

--- ConsoleApp1/32B.cs
+++ ConsoleApp1/32B.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
-    class Program
+    //class Program
+    //{
+    //static void Main(string[] args)
+    class Program32B
     {
-        static void Main(string[] args)
+        static void Main32B(string[] args)
         {
             var firstLine = Console.ReadLine();
             var result = "";
