caf6fb7198df57c62c69e1724d8bf337f3f6349f
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-02-28 14:50:05 +0100

    Added solution 1283A

--- ConsoleApp1/1283A.cs
+++ ConsoleApp1/1283A.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
-    class Program
+    //class Program
+    //{
+    //static void Main(string[] args)
+    class Program1283A
     {
-        static void Main(string[] args)
+        static void Main1283A(string[] args)
         {
             var firstLine = Console.ReadLine();
             var examples = int.Parse(firstLine);
