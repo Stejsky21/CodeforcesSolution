627b6386858bf95ca45a56d7da3963dab42f882e
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-03-02 17:55:07 +0100

    Added solution 707A

--- ConsoleApp1/707A.cs
+++ ConsoleApp1/707A.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
-    class Program
+    //class Program
+    //{
+    //static void Main(string[] args)
+    class Program707A
     {
-        static void Main(string[] args)
+        static void Main707A(string[] args)
         {
             var firstLine = Console.ReadLine();
             var X = int.Parse(firstLine.Split(' ')[0]);
@@ -16,11 +19,11 @@ namespace ConsoleApp1
             for (int i = 0; i < X; i++)
             {
                 var secondLine = Console.ReadLine();
-                var fotky = secondLine.Split(' ');
+                var photos = secondLine.Split(' ');
 
                 for (int j = 0; j < Y; j++)
                 {
-                    if (fotky[j] == "B" || fotky[j] == "W" || fotky[j] == "G")
+                    if (photos[j] == "B" || photos[j] == "W" || photos[j] == "G")
                     {
                     }
                     else
